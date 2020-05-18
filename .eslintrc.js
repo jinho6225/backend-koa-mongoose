729dc36b8e61c8c4509e3c718934a6792db17c61
jinho6225 <jinho6225@hotmail.com>
2020-05-18 00:07:46 -0700

    server koa completed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,7 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 11,
-    sourceType: 'module',
-  },
-  plugins: ['react'],
+  extends: 'standard',
   rules: {
+    indent: ['error', 4],
     semi: ['error', 'always'],
     'no-trailing-spaces': 0,
     'keyword-spacing': 0,
