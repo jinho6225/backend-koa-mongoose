b7b66210613bf462e7d796ab495a89c95728d995
jinho6225 <jinho6225@hotmail.com>
2020-05-18 10:23:29 -0700

    create, read, delete method completed

--- src/api/books/books.controller.js
+++ src/api/books/books.controller.js
@@ -1,13 +1,72 @@
-exports.list = (ctx) => {
-  ctx.body = 'listed';
+const Book = require('models/book');
+
+exports.list = async (ctx) => {
+  let books;
+
+  try {
+    books = await Book.find().exec();
+  } catch (e) {
+    return ctx.throw(500, e);
+  }
+  ctx.body = books;
 };
 
-exports.create = (ctx) => {
-  ctx.body = 'created';
+exports.create = async (ctx) => {
+  const { title, authors, publishedDate, price, tags } = ctx.request.body;
+
+  const book = new Book({
+    title,
+    authors,
+    publishedDate,
+    price,
+    tags,
+  });
+
+  try {
+    await book.save();
+  } catch (e) {
+    return ctx.throw(500, e);
+  }
+
+  ctx.body = book;
 };
 
-exports.delete = (ctx) => {
-  ctx.body = 'deleted';
+exports.get = async (ctx) => {
+  const { id } = ctx.params;
+  let book;
+
+  try {
+    book = await Book.findById(id).exec();
+  } catch (e) {
+    if (e.name === 'CastError') {
+      ctx.status = 400;
+      return;
+    }
+    return ctx.throw(500, e);
+  }
+
+  if (!book) {
+    ctx.status = 404;
+    ctx.body = { message: 'book not found' };
+    return;
+  }
+
+  ctx.body = book;
+};
+
+exports.delete = async (ctx) => {
+  const { id } = ctx.params; // URL 파라미터에서 id 값을 읽어옵니다.
+
+  try {
+    await Book.findByIdAndRemove(id).exec();
+  } catch (e) {
+    if (e.name === 'CastError') {
+      ctx.status = 400;
+      return;
+    }
+  }
+
+  ctx.status = 204;
 };
 
 exports.replace = (ctx) => {
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 
 const Koa = require('koa');
 const Router = require('koa-router');
@@ -6,16 +7,24 @@ const app = new Koa();
 const router = new Router();
 const api = require('./api');
 
+const mongoose = require('mongoose');
+const bodyParser = require('koa-bodyparser');
+
+mongoose.Promise = global.Promise;
+
+mongoose.set('useUnifiedTopology', true);
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(() => console.log('Successfully connected to mongodb'))
+  .catch((e) => console.error(e));
+
+const port = process.env.PORT || 4000;
+
+app.use(bodyParser()); // 바디파서 적용, 라우터 적용코드보다 상단에 있어야합니다.
 
 router.use('/api', api.routes());
-
 app.use(router.routes()).use(router.allowedMethods());
 
-app.use(async (ctx) => {
-  ctx.body = 'Hello World';
-});
-
-app.listen(4000, () => {
-  console.log('koa server is working now');
-
+app.listen(port, () => {
+  console.log(`koa server is working now at ${port}`);
 });
