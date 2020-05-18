cb3ccf4ca0806f3c02ab62bacb6fd69777605684
jinho6225 <jinho6225@hotmail.com>
2020-05-18 16:58:41 -0700

    jwt completed

--- src/api/auth/auth.controller.js
+++ src/api/auth/auth.controller.js
@@ -41,6 +41,10 @@ exports.localRegister = async (ctx) => {
     ctx.throw(500, e);
   }
 
+  ctx.cookies.set('access_token', token, {
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  });
   ctx.body = account.profile; // 프로필 정보로 응답합니다.
 };
 
@@ -75,6 +79,17 @@ exports.localLogin = async (ctx) => {
     return;
   }
 
+  let token = null;
+  try {
+    token = await account.generateToken();
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+
+  ctx.cookies.set('access_token', token, {
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  });
   ctx.body = account.profile;
 };
 
@@ -85,5 +100,20 @@ exports.exists = async (ctx) => {
 
 // 로그아웃
 exports.logout = async (ctx) => {
-  ctx.body = 'logout';
+  ctx.cookies.set('access_token', null, {
+    maxAge: 0,
+    httpOnly: true,
+  });
+  ctx.status = 204;
+};
+
+exports.check = (ctx) => {
+  const { user } = ctx.request;
+
+  if (!user) {
+    ctx.status = 403; // Forbidden
+    return;
+  }
+
+  ctx.body = user.profile;
 };
