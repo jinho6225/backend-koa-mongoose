81430618245beb03e9ba17141b341e9479ccfe46
jinho6225 <jinho6225@hotmail.com>
2020-05-18 10:43:58 -0700

    CRUD completed with mongoose

--- src/api/books/books.controller.js
+++ src/api/books/books.controller.js
@@ -1,4 +1,9 @@
 const Book = require('models/book');
+const Joi = require('joi');
+const {
+  Types: { ObjectId },
+} = require('mongoose');
+//const ObjectId = require('mongoose').Types.ObjectId
 
 exports.list = async (ctx) => {
   let books;
@@ -69,10 +74,65 @@ exports.delete = async (ctx) => {
   ctx.status = 204;
 };
 
-exports.replace = (ctx) => {
-  ctx.body = 'replaced';
+exports.replace = async (ctx) => {
+  const { id } = ctx.params;
+
+  if (!ObjectId.isValid(id)) {
+    ctx.status = 400; // Bad Request
+    return;
+  }
+  const schema = Joi.object().keys({
+    title: Joi.string().required(),
+    authors: Joi.array().items(
+      Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().email().required(),
+      })
+    ),
+    publishedDate: Joi.date().required(),
+    price: Joi.number().required(),
+    tags: Joi.array().items(Joi.string().required()),
+  });
+
+  console.log(ctx.request.body);
+  const result = Joi.validate(ctx.request.body, schema);
+
+  if (result.error) {
+    ctx.status = 400; // Bad Request
+    ctx.body = result.error;
+    return;
+  }
+
+  let book;
+
+  try {
+    book = await Book.findByIdAndUpdate(id, ctx.request.body, {
+      upsert: true, // 이 값을 넣어주면 데이터가 존재하지 않으면 새로 만들어줍니다
+      new: true, // 이 값을 넣어줘야 반환하는 값이 업데이트된 데이터입니다. 이 값이 없으면 ctx.body = book 했을때 업데이트 전의 데이터를 보여줍니다.
+    });
+  } catch (e) {
+    return ctx.throw(500, e);
+  }
+  ctx.body = book;
 };
 
-exports.update = (ctx) => {
-  ctx.body = 'updated';
+exports.update = async (ctx) => {
+  const { id } = ctx.params;
+  if (!ObjectId.isValid(id)) {
+    ctx.status = 400;
+    return;
+  }
+
+  let book;
+
+  try {
+    book = await Book.findByIdAndUpdate(id, ctx.request.body, {
+      // upsert 의 기본값은 false 입니다.
+      new: true, // 이 값을 넣어줘야 반환하는 값이 업데이트된 데이터입니다. 이 값이 없으면 ctx.body = book 했을때 업데이트 전의 데이터를 보여줍니다.
+    });
+  } catch (e) {
+    return ctx.throw(500, e);
+  }
+
+  ctx.body = book;
 };
