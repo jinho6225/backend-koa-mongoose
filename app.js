7ea790d795eaf5dcb709cd884508c207c9995cb0
jinho6225 <jinho6225@hotmail.com>
2020-05-18 14:30:32 -0700

    CRUD completed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const router = require('./routes/todos');
 const app = express();
 const port = process.env.PORT || 4000;
 
@@ -14,10 +14,13 @@ app.use(bodyParser.json());
 // Node.js의 native Promise 사용
 mongoose.Promise = global.Promise;
 
+mongoose.set('useCreateIndex', true);
 mongoose.set('useNewUrlParser', true);
 mongoose
   .connect(process.env.MONGO_URI, { useUnifiedTopology: true })
   .then(() => console.log('Successfully connected to mongodb'))
   .catch((e) => console.error(e));
+
+app.use('/todos', router);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
